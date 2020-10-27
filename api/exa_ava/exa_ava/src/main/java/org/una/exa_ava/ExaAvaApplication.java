0417e8c947208c67c17c446378d0eb012d4a0b50
thonyva <anthony.vargas.alvarado@est.una.ac.cr>
2020-10-27 17:27:03 -0600

    Configuración inicial de la API.

---
+++ api/exa_ava/exa_ava/src/main/java/org/una/exa_ava/ExaAvaApplication.java
@@ -0,0 +1,13 @@
+package org.una.exa_ava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExaAvaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExaAvaApplication.class, args);
+	}
+
+}
---
+++ api/exa_ava/exa_ava/src/test/java/org/una/exa_ava/ExaAvaApplicationTests.java
@@ -0,0 +1,13 @@
+package org.una.exa_ava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExaAvaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
