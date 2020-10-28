4499a3c0ca4c930d18bf893dc0013b3c9f605216
thonyva <anthony.vargas.alvarado@est.una.ac.cr>
2020-10-28 15:12:30 -0600

    Creadas las entidades.

---
+++ api/exa_ava/exa_ava/src/main/java/org/una/exa_ava/entities/Tarea.java
@@ -0,0 +1,85 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.una.exa_ava.entities;
+
+import java.io.Serializable;
+import java.util.Date;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.PrePersist;
+import javax.persistence.PreUpdate;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+/**
+ *
+ * @author thony
+ */
+
+@Entity
+@Table(name = "exa_ava_tareas")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+
+public class Tarea implements Serializable {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "nombre", length = 100)
+    private String nombre;
+    
+    @Column
+    private boolean estado;
+    
+    @Column(name = "descripcion", length = 200)
+    private String descripcion;
+    
+    @Column(name = "fecha_registro", updatable = false)
+    @Temporal(TemporalType.DATE)
+    @Setter(AccessLevel.NONE)
+    private Date fechaRegistro;
+
+    @Column(name = "fecha_modificacion")
+    @Setter(AccessLevel.NONE)
+    @Temporal(TemporalType.DATE)
+    private Date fechaModificacion;
+    
+    @Column
+    private double importancia;
+    
+    @Column
+    private double urgencia;
+    
+    @Column(name = "procentaje_avance")
+    private double procentajeAvance;
+
+    private static final long serialVersionUID = 1L;
+
+    @PrePersist
+    public void prePersist() {
+        estado=true;
+        fechaRegistro = new Date();
+        fechaModificacion = new Date();
+    }
+
+    @PreUpdate
+    public void preUpdate() {
+        fechaModificacion = new Date();
+    }
+}
